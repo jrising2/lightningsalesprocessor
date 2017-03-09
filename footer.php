81017885de3cfc6e0c4e0ad74f427fa7cf492e46
hwoo2 <hwoo2@kent.edu>
2017-03-09 11:22:49 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,59 @@
+        <!-- Footer Start -->
+        <hr>
+        <footer>
+            <div class="row">
+                <div class="col-md-3">
+                    <h4><span class="glyphicon glyphicon-info-sign"></span> About Us</h4>
+                    <ul class="nav nav-stacked">
+                        <li><a href="#">About Book Store</a></li>
+                        <li><a href="#">Investor Relations</a></li>
+                        <li><a href="#">Careers at Book Store</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h4><span class="glyphicon glyphicon-earphone"></span> Customer Service</h4>
+                    <ul class="nav nav-stacked">
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">In-Store Pickup</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Terms and Conditions</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h4><span class="glyphicon glyphicon-link"></span> Links</h4>
+                    <ul class="nav nav-stacked">
+                        <li><a href="#">Kent State University</a></li>
+                        <li><a href="#">Barnes And Noble</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h4><span class="glyphicon glyphicon-envelope"></span> Contact us</h4>
+                    <form role="form">
+                        <div class="form-group tight-form-group">
+                            <label class="sr-only" for="name">Name</label>
+                            <input type="text" class="form-control" name="email" placeholder="Enter name ...">
+                        </div>
+                        <div class="form-group tight-form-group">
+                            <label class="sr-only" for="email">Email</label>
+                            <input type="email" class="form-control" name="email" placeholder="Enter email ...">
+                        </div>
+                        <div class="form-group tight-form-group">
+                            <label class="sr-only" for="email">Email</label>
+                            <textarea class="form-control" rows="3" placeholder="Enter message ..."></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
+
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </div>
+</body>
+
+</html>
