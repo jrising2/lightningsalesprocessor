1eebb8d60109bddcb28f51fc287dbacbcfb177b4
jrising2 <jrising2@kent.edu>
2017-04-19 17:20:18 -0400

    Added about page

---
+++ about.php
@@ -0,0 +1,56 @@
+<?php
+include_once "includes/header.php";
+?>
+
+<!-- Main Body Start -->
+<h1 align="center">Kent State CS Capstone Project</h1>
+<h3 align="center">Spring 2017</h3>
+
+<div class="well">
+<h3>Project Title</h3>
+<p>Lightning Sales Processor</p>
+<h3>Project Description</h3>
+<p>Web based order tracking system that allows employees to track and manage online orders placed by customers.</p>
+<p>To view employee system, follow the link below and login with ID: 3, and password: robin</p>
+<a href="emp/index.php">Employee System</a>
+</div>
+
+<div class="container">
+  <h2>Team Members</h2>          
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Contributions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Hawon Woo</td>
+        <td>Login, Registration, Transaction, Security</td>
+      </tr>
+      <tr>
+        <td>Chase Walkup</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>Rebecca Chase</td>
+        <td>july@example.com</td>
+      </tr>
+      <tr>
+        <td>Juleyn Arnold</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>Joshua Rising</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<!-- Main Body End -->
+
+<?php
+include_once "includes/footer.php";
+?>
